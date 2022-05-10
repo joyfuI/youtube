eaa87977a568ed9cdf3e631cbe7c6860932ccea1
joyfuI <1342862+joyfuI@users.noreply.github.com>
2022-05-10 22:16:09 +0900

    iife로 변경

--- static/youtube_setting.js
+++ static/youtube_setting.js
@@ -1,27 +1,31 @@
 'use strict';
 
-const default_save_path = document.getElementById('default_save_path');
-const default_save_path_btn = document.getElementById('default_save_path_btn');
-const cookiefile_path = document.getElementById('cookiefile_path');
-const cookiefile_path_btn = document.getElementById('cookiefile_path_btn');
-
-// 기본 저장 폴더 경로 선택
-default_save_path_btn.addEventListener('click', (event) => {
-  event.preventDefault();
-  m_select_local_file_modal(
-    '저장 경로 선택',
-    default_save_path.value,
-    true,
-    (result) => {
-      default_save_path.value = result;
-    }
+(() => {
+  const default_save_path = document.getElementById('default_save_path');
+  const default_save_path_btn = document.getElementById(
+    'default_save_path_btn'
   );
-});
+  const cookiefile_path = document.getElementById('cookiefile_path');
+  const cookiefile_path_btn = document.getElementById('cookiefile_path_btn');
 
-// cookiefile 파일 선택
-cookiefile_path_btn.addEventListener('click', (event) => {
-  event.preventDefault();
-  m_select_local_file_modal('파일 선택', path_data, false, (result) => {
-    cookiefile_path.value = result;
+  // 기본 저장 폴더 경로 선택
+  default_save_path_btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    m_select_local_file_modal(
+      '저장 경로 선택',
+      default_save_path.value,
+      true,
+      (result) => {
+        default_save_path.value = result;
+      }
+    );
   });
-});
+
+  // cookiefile 파일 선택
+  cookiefile_path_btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    m_select_local_file_modal('파일 선택', path_data, false, (result) => {
+      cookiefile_path.value = result;
+    });
+  });
+})();
